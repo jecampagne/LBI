c938b8d52d6d13f6a3ab86fb0ecd518439104a74
John Tamanas <8213449+jtamanas@users.noreply.github.com>
2022-01-13 07:22:08 -0800

    Working!

--- lbi/diagnostics/roc_auc.py
+++ lbi/diagnostics/roc_auc.py
@@ -4,8 +4,8 @@ import numpy as onp
 import optax
 import sklearn.metrics as skm
 import torch
-from lbi.models import get_train_step, get_valid_step
-# from lbi.models.classifier import InitializeClassifier
+from lbi.models import get_train_step, get_valid_step, parallel_init_fn
+from lbi.models.classifier import construct_Classifier
 from lbi.trainer import getTrainer
 from sklearn.model_selection import train_test_split
 
@@ -33,8 +33,9 @@ def data_loader_builder(a_samples, b_samples, train_split=0.9):
 
     DSet = torch.utils.data.TensorDataset
 
-    trainDataset = DSet(train_data, train_labels)
-    validDataset = DSet(valid_data, valid_labels)
+    # TODO: adapt classifier to work without Theta (second arg)
+    trainDataset = DSet(train_data, train_data, train_labels)
+    validDataset = DSet(valid_data, valid_data, valid_labels)
 
     train_dataloader = torch.utils.data.DataLoader(
         trainDataset,
@@ -62,53 +63,42 @@ def train_classifier(
     num_layers=3,
     hidden_dim=128,
 ):
-    def loss(params, inputs, label):
-        """binary cross entropy with logits
-        taken from jaxchem
-
-        default loss takes context, so we specify this one to circumvent that
-        """
-        label = label.squeeze()
-        # log ratio is the logit of the discriminator
-        l_d = logit_d(params, inputs).squeeze()
-        max_val = np.clip(-l_d, 0, None)
-        L = (
-            l_d
-            - l_d * label
-            + max_val
-            + np.log(np.exp(-max_val) + np.exp((-l_d - max_val)))
-        )
-        return np.mean(L)
-
-    # --------------------------
-    # Create model
-    model_params, _, logit_d = InitializeClassifier(
-        model_rng=rng_key,
-        obs_dim=a_samples.shape[-1],
-        theta_dim=0,
-        num_layers=num_layers,
-        hidden_dim=hidden_dim,
-    )
-
     # --------------------------
     # Create optimizer
     optimizer = optax.chain(
         # Set the parameters of Adam optimizer
         optax.adamw(
             learning_rate=learning_rate,
-            weight_decay=1e-2,
+            weight_decay=1e-1,
             b1=0.9,
             b2=0.999,
             eps=1e-8,
         ),
+        optax.adaptive_grad_clip(1e-2),
     )
-    opt_state = optimizer.init(model_params)
+
+    # --------------------------
+    # Create model
+    model, log_prob, loss_fn = construct_Classifier(
+        num_layers=num_layers,
+        hidden_dim=hidden_dim,
+        use_residual=True,
+        act="celu",
+    )
+
+    params, opt_state = parallel_init_fn(
+        jax.random.split(rng_key, 1),
+        model,
+        optimizer,
+        (a_samples.shape[-1],),
+        (a_samples.shape[-1],),
+    )
 
     # --------------------------
     # Create trainer
 
-    train_step = get_train_step(loss, optimizer)
-    valid_step = get_valid_step({"valid_loss": loss, "also_valid_loss": loss})
+    train_step = get_train_step(loss_fn, optimizer)
+    valid_step = get_valid_step({"valid_loss": loss_fn})
 
     trainer = getTrainer(
         train_step,
@@ -126,13 +116,13 @@ def train_classifier(
     )
 
     model_params = trainer(
-        model_params,
+        params,
         opt_state,
         train_dataloader,
         valid_dataloader=valid_dataloader,
     )
 
-    return model_params, logit_d
+    return model_params, log_prob
 
 
 def distinguish_samples(a_samples, b_samples, classifier_params, logit_d):
@@ -141,8 +131,9 @@ def distinguish_samples(a_samples, b_samples, classifier_params, logit_d):
     samples = np.vstack([a_samples, b_samples])
     true_labels = np.vstack([a_labels, b_labels])
 
-    pred_labels = logit_d(classifier_params, samples)
-    pred_labels = jax.nn.sigmoid(pred_labels)
+    parallel_logit_d = jax.vmap(logit_d, in_axes=(0, None, None))
+    pred_labels = parallel_logit_d(classifier_params, samples, samples)
+    pred_labels = jax.nn.sigmoid(pred_labels).mean(0)
     return true_labels, pred_labels
 
 
